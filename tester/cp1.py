e4b64d44a05cd62b826d7cddb534d84995163aa4
Tom Kuipers <tom.kuipers@rhul.ac.uk>
2022-01-05 17:46:02 +0000

    Changing config to make compatible for Google Colab

--- tester/cp1.py
+++ tester/cp1.py
@@ -34,5 +34,4 @@ test = {
             ]
         }
     ],
-    'scored': True
 }
--- tester/import.py
+++ tester/import.py
@@ -7,14 +7,13 @@ test = {
                 {
                     'code': r"""
                     >>> # Numpy has not been imported!
-		    >>> # Check that you have executed the 'import numpy as np' code cell!
+                    >>> # Check that you have executed the 'import numpy as np' code cell!
                     >>>
-		    >>> np is not None
+                    >>> np is not None
                     True
                     """
                 }
             ]
         }
     ],
-    'scored': True
 }
